517af78d88cbbda25dabc2ea9e9ec4ff9ceb6fec
Admin <admin@example.com>
2026-04-23 00:56:33 +0700

    Force CORS headers in index.php

--- backend/public/index.php
+++ backend/public/index.php
@@ -1,5 +1,13 @@
 <?php
 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+
+if (isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    exit;
+}
+
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
