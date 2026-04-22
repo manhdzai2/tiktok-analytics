b0d3a3904281fea5605fad5be79db887a242647b
Admin <admin@example.com>
2026-04-23 01:46:58 +0700

    Remove duplicate CORS headers

--- backend/public/index.php
+++ backend/public/index.php
@@ -1,15 +1,5 @@
 <?php
 
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With, Accept, Origin');
-header('Access-Control-Max-Age: 86400');
-
-if (isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-    header('HTTP/1.1 200 OK');
-    exit;
-}
-
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
